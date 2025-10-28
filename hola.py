57ae666d146bfb7a5bf51edc9b26aced0dff1e06
Cohorte 4 <cadenajuanjose68@gmail.com>
2025-10-28 11:53:59 -0500

    mi primer commit

---
+++ hola.py
@@ -0,0 +1,8 @@
+nombre = input("ingresa tu nombre:")
+edad = int(input("ingresa tu edad:"))
+if edad >=18:
+    print(f"mi monbre es:{nombre}, tnego {edad} años y soy mayor de edad")
+     
+
+else:
+    print(f"mi nombre es:{nombre}, tengo {edad} años soy menor de edad")
